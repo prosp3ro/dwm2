10cc68d1ba14faabe51e54b091ccc8ddfedf0a08
prosp3ro <g0lden_vo1d@protonmail.com>
2024-05-05 10:23:58 +0200

    config tweaks

--- config.h
+++ config.h
@@ -3,13 +3,14 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+
+static const unsigned int gappih    = 7;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -148,22 +149,10 @@ static const Key keys[] = {
     { MODKEY,                XK_comma,           viewprev,            {0} },
     { MODKEY,                XK_Escape,          viewprev,            {0} },
     { MODKEY|ShiftMask,      XK_comma,           tagtoprev,           {0} },
-	// { MODKEY,              XK_z,      incrgaps,       {.i = +1 } },
-	// { MODKEY,              XK_x,      incrgaps,       {.i = -1 } },
-	{ MODKEY,    XK_z,      incrogaps,      {.i = +1 } },
-	{ MODKEY,    XK_x,      incrogaps,      {.i = -1 } },
-	{ MODKEY|ShiftMask,  XK_z,      incrigaps,      {.i = +1 } },
-	{ MODKEY|ShiftMask,  XK_x,      incrigaps,      {.i = -1 } },
-	{ MODKEY,              XK_a,      togglegaps,     {0} },
-	{ MODKEY|ShiftMask,    XK_a,      defaultgaps,    {0} },
-	// { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-	// { MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-	// { MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-	// { MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	// { MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	// { MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-	// { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-	// { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
+	{ MODKEY,                XK_z,               incrgaps,            {.i = +2 } },
+	{ MODKEY,                XK_x,               incrgaps,            {.i = -2 } },
+	{ MODKEY,                XK_a,               togglegaps,          {0} },
+	{ MODKEY|ShiftMask,      XK_a,               defaultgaps,         {0} },
     // { MODKEY,                XK_z,               zoom,                {0} },
     // { MODKEY,                XK_comma,           focusmon,            {.i = -1 } },
     // { MODKEY,                XK_period,          focusmon,            {.i = +1 } },
