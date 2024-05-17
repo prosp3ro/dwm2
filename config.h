879017c27d98ceb584ff73fc477e4e3ee80306e6
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-05-17 20:22:46 +0200

    sync

--- config.h
+++ config.h
@@ -155,8 +155,6 @@ static const Key keys[] = {
     // { MODKEY,                XK_period,          focusmon,            {.i = +1 } },
     // { MODKEY|ShiftMask,      XK_comma,           tagmon,              {.i = -1 } },
     // { MODKEY|ShiftMask,      XK_period,          tagmon,              {.i = +1 } },
-    { MODKEY,                XK_s,               view,                {.ui = 1 << 7} },
-    { MODKEY,                XK_e,               view,                {.ui = 1 << 8} },
     TAGKEYS(                 XK_1,                                    0)
     TAGKEYS(                 XK_2,                                    1)
     TAGKEYS(                 XK_3,                                    2)
