cd37ef2ab041ce32c6b95db2db571b871211f068
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-05-19 14:40:17 +0200

    reverse vanitygaps patch

--- config.h
+++ config.h
@@ -6,12 +6,6 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const unsigned int gappih    = 7;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -19,7 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray             = 1;        /* 0 means no systray */
 
 static char *fonts[] = { 
-    "monospace:size=10",
+    "monospace:size=9",
     "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true"
 };
 
